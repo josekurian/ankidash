0f2b930c42bb711494872dcb0bacdcad74a0187f
oraclespainpresales <oraclespainpresales@gmail.com>
2019-10-09 00:42:05 +0200

    log

--- js/viewModels/dashboard.js
+++ js/viewModels/dashboard.js
@@ -308,6 +308,7 @@ function socketioController() {
 	);
 
 	socket.on('speed', function(msg, callback) {
+		console.log(msg);
 				if (!racing) return;
 				msg.forEach(function(element) {
 					  updateSpeed(element.payload.data.data_carname, element.payload.data.data_speed);
